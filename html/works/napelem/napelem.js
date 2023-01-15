bc3165ac6542f75e441d267858eda2a3522063fd
ParaghT <118506519+ParaghT@users.noreply.github.com>
2023-01-15 15:15:26 +0100

    Add files via upload

---
+++ html/works/napelem/napelem.js
@@ -0,0 +1,9 @@
+function panelek(db){
+    const panelek=document.getElementById('panelek');
+    panelek.innerHTML="";
+    for (i = 0; i < db; i++) {
+        panelek.innerHTML += "<img src='kepek/napelem_ikon.png'>"; 
+    }
+    panelek.innerHTML += "("+db+" db)";
+    document.getElementById('osszteljesitmeny').value = db * 275;
+}
